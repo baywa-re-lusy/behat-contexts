eb6f6abcaee9b5b8fedce4e1728208d239c5024c
Pascal Paulis <ppaulis@gmail.com>
2026-01-09 09:17:58 +0100

    added test step to check last console route interaction
    Signed-off-by: Pascal Paulis <ppaulis@gmail.com>

--- src/BehatContext/ConsoleContext.php
+++ src/BehatContext/ConsoleContext.php
@@ -78,4 +78,29 @@ class ConsoleContext implements Context
             throw new \Exception('Timestamp is invalid or too old.');
         }
     }
+
+    /**
+     * @Then the file :filename should contain a timestamp not older than :nbSeconds seconds
+     * @throws Exception
+     */
+    public function theFileShouldContainATimestampNotOlderThanSeconds(string $filename, int $nbSeconds): void
+    {
+        $filePath = getcwd() . DIRECTORY_SEPARATOR . $filename;
+
+        if (!file_exists($filePath)) {
+            throw new Exception('File does not exist.');
+        }
+
+        $fileContent = file_get_contents($filePath);
+
+        if (!is_string($fileContent) || !ctype_digit($fileContent)) {
+            throw new Exception('No timestamp found in file : ' . var_export($fileContent, true));
+        }
+
+        $secondsPassed = time() - (int)$fileContent;
+
+        if ($secondsPassed > $nbSeconds) {
+            throw new Exception('Timestamp is invalid or too old.');
+        }
+    }
 }
