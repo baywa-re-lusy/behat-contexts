5b13a06ebdf7a7c0af03b28cce6a8dc4b99cbcd2
Pascal Paulis <ppaulis@gmail.com>
2026-02-10 17:49:18 +0100

    added test step to verify multiple queue messages
    Signed-off-by: Pascal Paulis <ppaulis@gmail.com>

--- src/BehatContext/SqsContext.php
+++ src/BehatContext/SqsContext.php
@@ -164,11 +164,12 @@ class SqsContext implements Context
      */
     public function aMessageInQueue(string $queueName, TableNode $message): void
     {
+        /** @var array<string, string> $data */
         $data = $message->getRowsHash();
 
         foreach ($data as $key => &$value) {
-            if (str_starts_with((string)$value, 'json://')) {
-                $value = json_decode(substr((string)$value, 7), true);
+            if (str_starts_with($value, 'json://')) {
+                $value = json_decode(substr($value, 7), true);
             }
         }
 
